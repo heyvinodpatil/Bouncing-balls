e2152368f1e92ad390c2997c240727a3c7d3ad58
heyvinodpatil <heyvinodpatil@gmail.com>
2024-10-11 13:15:11 +0530

    Added color updates to range slider as well

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Balls color change
+// Balls color change and range accent color update
 const colorPicker1 = document.querySelector(".color-picker1");
 colorPicker1.addEventListener("input", updateColor.bind(this, "updateColor1"));
 
@@ -14,22 +14,27 @@ colorPicker4.addEventListener("input", updateColor.bind(this, "updateColor4"));
 const colorPicker5 = document.querySelector(".color-picker5");
 colorPicker5.addEventListener("input", updateColor.bind(this, "updateColor5"));
 
-function bgColorChange(element) {
+function bgColorChange(element, rangeClass) {
     const ballColor = document.querySelector(element);
-    const ballBgColor = ballColor.style.backgroundColor = event.target.value;
+    const newColor = event.target.value;
+    ballColor.style.backgroundColor = newColor;
+
+    // Change the accent color of the corresponding range input
+    const range = document.querySelector(rangeClass);
+    range.style.accentColor = newColor;
 }
 
 function updateColor(color) {
     if (color === "updateColor1") {
-        bgColorChange("li:nth-child(1)");
+        bgColorChange("li:nth-child(1)", ".range1");
     } else if (color === "updateColor2") {
-        bgColorChange("li:nth-child(2)");
+        bgColorChange("li:nth-child(2)", ".range2");
     } else if (color === "updateColor3") {
-        bgColorChange("li:nth-child(3)");
+        bgColorChange("li:nth-child(3)", ".range3");
     } else if (color === "updateColor4") {
-        bgColorChange("li:nth-child(4)");
+        bgColorChange("li:nth-child(4)", ".range4");
     } else {
-        bgColorChange("li:nth-child(5)");
+        bgColorChange("li:nth-child(5)", ".range5");
     }
 }
 
@@ -40,34 +45,30 @@ const range3 = document.querySelector('.range3');
 const range4 = document.querySelector('.range4');
 const range5 = document.querySelector('.range5');
 
-
 function rangeValue(value) {
+    let newValue;
+    let target;
+    let finalTarget;
+
     if (value === "range1") {
-        let newValue = range1.value;
-        const target = document.querySelector('li:nth-child(1)');
-        let finalTarget = "1." + newValue + "s";
-        target.style.animationDuration = finalTarget;
+        newValue = range1.value;
+        target = document.querySelector('li:nth-child(1)');
     } else if (value === "range2") {
-        let newValue = range2.value;
-        const target = document.querySelector('li:nth-child(2)');
-        let finalTarget = "1." + newValue + "s";
-        target.style.animationDuration = finalTarget;
+        newValue = range2.value;
+        target = document.querySelector('li:nth-child(2)');
     } else if (value === "range3") {
-        let newValue = range1.value;
-        const target = document.querySelector('li:nth-child(3)');
-        let finalTarget = "1." + newValue + "s";
-        target.style.animationDuration = finalTarget;
+        newValue = range3.value;
+        target = document.querySelector('li:nth-child(3)');
     } else if (value === "range4") {
-        let newValue = range3.value;
-        const target = document.querySelector('li:nth-child(4)');
-        let finalTarget = "1." + newValue + "s";
-        target.style.animationDuration = finalTarget;
+        newValue = range4.value;
+        target = document.querySelector('li:nth-child(4)');
     } else {
-        let newValue = range5.value;
-        const target = document.querySelector('li:nth-child(5)');
-        let finalTarget = "1." + newValue + "s";
-        target.style.animationDuration = finalTarget;
+        newValue = range5.value;
+        target = document.querySelector('li:nth-child(5)');
     }
+
+    finalTarget = "1." + newValue + "s";
+    target.style.animationDuration = finalTarget;
 }
 
 range1.addEventListener("input", rangeValue.bind(this, "range1"));
